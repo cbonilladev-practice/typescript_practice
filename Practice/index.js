97a057d9f95610493006137d209d597a22a1e9c3
cabonilla <christian.bonilla01@gmail.com>
2021-08-13 09:15:36 -0700

    Initial commit.

---
+++ Practice/index.js
@@ -0,0 +1,25 @@
+const person = {
+    first: 'Jeff',
+    last: 'Delaney'
+};
+const person2 = {
+    first: 'Usain',
+    last: 'Bolt',
+    fast: true
+};
+function pow(x, y) {
+    return Math.pow(x, y).toString();
+}
+const arr = [];
+arr.push(1);
+arr.push('23');
+arr.push(false);
+class Observable {
+    value;
+    constructor(value) {
+        this.value = value;
+    }
+}
+let x;
+let y;
+let z = new Observable(23);
---
+++ Practice/index.ts
@@ -0,0 +1,36 @@
+interface Person {
+	first: string;
+	last: string;
+	[key: string]: any
+}
+
+const person: Person = {
+	first: 'Jeff',
+	last: 'Delaney'
+}
+
+const person2: Person = {
+	first: 'Usain',
+	last: 'Bolt',
+	fast: true
+}
+
+function pow(x: number, y: number): string {
+	return Math.pow(x,y).toString();
+}
+
+type MyList = [number?, string?, boolean?]
+
+const arr: MyList =  []
+
+arr.push(1)
+arr.push('23')
+arr.push(false)
+
+class Observable<T> {
+	constructor(public value: T) {}
+}
+
+let x: Observable<number>;
+let y: Observable<Person>;
+let z = new Observable(23)
